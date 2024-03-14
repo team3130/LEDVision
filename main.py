d47c7b632f35c588680891f6c449b9c1ff702c79
Isaac <IsaacCA2018@gmail.com>
2024-03-14 16:17:53 -0500

    finished prototype

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
 import pygame
-from cv2 import *
+import cv2
 
-screen = pygame.display.set_mode((10, 100))
+running = True
+pixelSize = 50
+numOfLeds = 30 #dont make this larger than the width of the image
+times = 0
+
+screen = pygame.display.set_mode((pixelSize * numOfLeds, pixelSize))
 pygame.display.set_caption('LEDs')
 
-cam = VideoCapture(0)
+cam = cv2.VideoCapture(0)
 
-result, img = cam.read()
-img = img[0:11]
+while running:
 
-while True:
-    result, img = cam.read() 
+    result, img = cam.read()
+    img = img[(numOfLeds + 1) * -1: -1]
+    img = cv2.flip(img, 1)
     
     if result:
-        img = cv2.resize(img, (10, 1))
+        img = cv2.resize(img, (numOfLeds, 1))
 
         for i in range(len(img)):
-            pygame.draw.rect(screen, img[0][i], pygame.Rect(i * 10, i * 10, 10, 10))
+            for j in range(len(img[i])):
+                pygame.draw.rect(screen, (img[i][j][2], img[i][j][1], img[i][j][0]), pygame.Rect(j * pixelSize, i * pixelSize, pixelSize, pixelSize))
             
         pygame.display.flip()
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:  
+           running = False
