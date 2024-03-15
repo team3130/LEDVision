ca349c43bb4b0f512d62314b376be05759288ea2
Robot1235369 <73447037+Robot1235369@users.noreply.github.com>
2024-03-15 11:49:03 -0500

    Create picamera.py

---
+++ picamera.py
@@ -0,0 +1,41 @@
+import pygame # Use command 'pip install pygame'
+from picamera import Picamera # Use command 'pip install picamera'
+from numpy import np # Python comes with this so you're fine
+import cv2 # Use command 'pip install 'opencv-python'
+
+running = True
+pixelSize = 50
+numOfLeds = 30 # Don't make this larger than the width of the image
+
+# Creates the pygame window
+screen = pygame.display.set_mode((pixelSize * numOfLeds, pixelSize))
+pygame.display.set_caption('LEDs')
+
+# Creates the camera and image array
+camera = Picamera()
+img = np.empty((240, 320, 3), dtype=np.uint8)
+
+while running:
+
+    # Reads image, crops it, and flips it
+    camera.capture(img, 'rgb')
+    img = img[(numOfLeds + 1) * -1: -1]
+    img = cv2.flip(img, 1) # This is only for testing. Remove this in implementation
+  
+    # Shrinks and pixelates the image
+    img = cv2.resize(img, (numOfLeds, 1))
+
+    # Assigns the pixels in img to pygame Rects
+    # Remember that opencv (cv2) uses BRG and pygame uses RGB
+    for i in range(len(img)):
+        for j in range(len(img[i])):
+            pxl = pygame.Rect(j * pixelSize, i * pixelSize, pixelSize, pixelSize)
+            pygame.draw.rect(screen, (img[i][j][2], img[i][j][1], img[i][j][0]), pxl)
+        
+    # Draws everything to the screen
+    pygame.display.flip()
+    
+    # Makes the pygame 'X' work
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:  
+           running = False
